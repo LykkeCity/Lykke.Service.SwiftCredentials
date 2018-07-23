37e3dca48b4e5ed44294ab6330f79c4e13199a8c
nikagamkrelidze <nikagamkrelidze233@gmail.com>
2018-07-23 13:58:12 +0400

    bounded context added

--- src/Lykke.Service.SwiftCredentials/CqrsModule.cs
+++ src/Lykke.Service.SwiftCredentials/CqrsModule.cs
@@ -1,17 +1,44 @@
-﻿using Autofac;
+﻿using System.Collections.Generic;
+using Autofac;
+using Castle.MicroKernel;
+using Common.Log;
+using JetBrains.Annotations;
+using Lykke.Messaging;
+using Lykke.Messaging.RabbitMq;
 
 namespace Lykke.Service.SwiftCredentials
 {
+    [UsedImplicitly]
     public class CqrsModule : Module
     {
-        public CqrsModule()
+        private ILog _log;
+        
+        public CqrsModule(
+            ILog log)
         {
-            
+            _log = log;
         }
         
         protected override void Load(ContainerBuilder builder)
         {
-            
+            /*
+            string selfRoute = "self";
+            string commandsRoute = "commands";
+            string eventsRoute = "events";
+            Messaging.Serialization.MessagePackSerializerFactory.Defaults.FormatterResolver = MessagePack.Resolvers.ContractlessStandardResolver.Instance;
+            var rabbitMqSettings = new RabbitMQ.Client.ConnectionFactory { Uri = _settings.CurrentValue.MessagesJob.Transports.ClientRabbitMqConnectionString };
+            var rabbitMqSagasSettings = new RabbitMQ.Client.ConnectionFactory { Uri = _settings.CurrentValue.SagasRabbitMq.RabbitConnectionString };
+
+            builder.Register(context => new AutofacDependencyResolver(context)).As<IDependencyResolver>();
+
+            var messagingEngine = new MessagingEngine(_log,
+                new TransportResolver(new Dictionary<string, TransportInfo>
+                {
+                    { "SagasRabbitMq", new TransportInfo(rabbitMqSagasSettings.Endpoint.ToString(), rabbitMqSagasSettings.UserName, rabbitMqSagasSettings.Password, "None", "RabbitMq") },
+                    { "ClientRabbitMq", new TransportInfo(rabbitMqSettings.Endpoint.ToString(), rabbitMqSettings.UserName, rabbitMqSettings.Password, "None", "RabbitMq") }
+                }),
+                new RabbitMqTransportFactory());
+                */
         }
     }
 }
